70dfb545fc3ab758c7c1ee184352bf37e37fc638
Archil Lelashvili <lelashvili.a@northeastern.edu>
2023-11-05 15:36:14 -0500

    Random numbers & Java faker

--- src/main/java/info5001/ui/RangePricingApplication.java
+++ src/main/java/info5001/ui/RangePricingApplication.java
@@ -5,6 +5,10 @@
  */
 package info5001.ui;
 
+import java.util.Random;
+
+import com.github.javafaker.Faker;
+
 import info5001.model.Business.Business;
 import info5001.model.Business.ConfigureABusiness;
 import info5001.model.ProductManagement.ProductCatalog;
@@ -24,10 +28,50 @@ public class RangePricingApplication {
   public static void main(String[] args) {
     // TODO code application logic here
 
-    Business business = ConfigureABusiness.createABusinessAndLoadALotOfData("Amazon", 20, 10, 30, 100, 10);
-    business.printShortInfo();
+    // Random numbers
+
+    Random randomInstance = new Random();
+
+    System.out.println(" Random Numbers: ");
+
+    for (int i = 0; i < 10; i++) {
+      int randomInt = randomInstance.nextInt(100);
+      // Random 10-15
+      int randomIntInRange10to15 = randomInstance.nextInt(6) + 10;
+      float randomFloat = randomInstance.nextFloat();
+      System.out.println("Random integer: " + randomInt);
+      System.out.println("Random float: " + randomFloat);
+    }
+
+    // Data that is not number (e.g. names, addresses, etc.)
+
+    Faker fakerInstance = new Faker();
+
+    System.out.println(" Customer list: ");
+    for (int i = 0; i < 200; i++) {
+
+      String fullName = fakerInstance.name().fullName();
+      String address = fakerInstance.address().fullAddress();
+
+      System.out.println(fullName);
+      System.out.println(address);
+      System.out.println("-----------------");
+    }
+
+    System.out.println(" Supplier list: ");
+    for (int i = 0; i < 200; i++) {
+
+      String companyName = fakerInstance.company().name();
+
+      System.out.println(companyName);
+      System.out.println("-----------------");
+    }
+
+    // Business business =
+    // ConfigureABusiness.createABusinessAndLoadALotOfData("Amazon", 20, 10, 30,
+    // 100, 10);
+    // business.printShortInfo();
 
   }
-
 
 }
